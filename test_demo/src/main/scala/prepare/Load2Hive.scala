b77b8824ec71dbbe7ecb8b45f2513d2f2a6cf075
fansy <fansy1990@foxmail.com>
2018-12-21 10:01:45 +0800

    添加分区支持

--- test_demo/src/main/scala/prepare/Load2Hive.scala
+++ test_demo/src/main/scala/prepare/Load2Hive.scala
@@ -10,18 +10,18 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
   */
 object Load2Hive {
   def main(args: Array[String]): Unit = {
-    if(args.length !=4){
-      println("Usage: prepare.Load2Hive <input> <hive_table> <times> <appName>")
+    if(args.length !=5){
+      println("Usage: prepare.Load2Hive <input> <hive_table> <times> <appName> <partitionColumn>")
       System.exit(-1)
     }
     //
-    val (input, table, num , appName) = (args(0),args(1), args(2).toInt,args(3))
+    val (input, table, num , appName, partitionColumn) = (args(0),args(1), args(2).toInt,args(3),args(4))
 
     val spark = SparkSession.builder().appName(appName).enableHiveSupport().getOrCreate()
 
     val df =spark.read.format("csv").option("header", "true").option("inferSchema","true").load(input)
 
-    Array.fill[DataFrame](num)(df).reduce(_ union _).write.mode(SaveMode.Overwrite).saveAsTable(table)
+    Array.fill[DataFrame](num)(df).reduce(_ union _).write.partitionBy(partitionColumn).mode(SaveMode.Overwrite).saveAsTable(table)
 
     spark.stop()
   }
