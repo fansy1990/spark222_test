055fa4ec9407f5cb1726afc532e711b34200172b
fansy <fansy1990@foxmail.com>
2018-12-21 17:17:36 +0800

    添加时间备注

--- test_demo/src/main/java/statics/DescribeStaticsDemo.java
+++ test_demo/src/main/java/statics/DescribeStaticsDemo.java
@@ -12,15 +12,26 @@ import java.io.IOException;
  * @Email: fansy1990@foxmail.com
  */
 public class DescribeStaticsDemo {
+    static String mainClass = "statics.DescribeStatics";
     public static void main(String[] args) throws IOException {
 //        Prepare.uploadJar();
-        first();
+//        first();
+        second();
     }
 
     public static void first() {
-        String mainClass = "statics.DescribeStatics";
         String appName = "demo_30m statics";
         String[] arguments = {"default.demo_30m", "default.demo_30m_statics", appName};
+        Args innerArgs = Args.getArgs(appName, mainClass, arguments, EngineType.SPARK);
+        SparkYarnJob.runAndMonitor(innerArgs);
+    }
+
+    /**
+     * 1个 ApplicationMaster ； 2个子节点Executor； 耗时4.7 mins
+     */
+    public static void second(){
+        String appName = "demo_600w statics";
+        String[] arguments = {"default.demo_600w", "default.demo_600w_statics", appName};
         Args innerArgs = Args.getArgs(appName, mainClass, arguments, EngineType.SPARK);
         SparkYarnJob.runAndMonitor(innerArgs);
     }
